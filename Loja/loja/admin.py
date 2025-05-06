8473deab27c8d76937faf3fe46070b7f4bad9650
Marco Antonio Cavalcanti da Silva <20231011110033@ifrn.local>
2025-05-06 14:15:37 -0300

    aula 7 - trabalhando com área administrativa

--- Loja/loja/admin.py
+++ Loja/loja/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin #isso já vai estar existindo no arquivo
 # Register your models here.
 from .models import * #imporata nossos models
-admin.site.register(Fabricante) #adiciona a interface do adm
+
+class FabricanteAdmin(admin.ModelAdmin):
+    # Cria um filtro de hierarquia com datas
+    date_hierarchy = 'criado_em'
+
+class ProdutoAdmin(admin.ModelAdmin):
+    date_hierarchy = 'criado_em'
+    list_display = ('Produto', 'destaque', 'promocao', 'msgPromocao', 'preco', 'categoria',)
+    empty_value_display = 'Vazio'
+    fields = ('Produto', 'destaque', 'promocao', 'preco', 'categoria',) #'msgPromocao' foi tirado pois não se usa fields e exclude ao mesmo tempo
+    search_fields = ('Produto',)
+    exclude = ('msgPromocao',)
+
+
+admin.site.register(Fabricante,FabricanteAdmin) #adiciona a interface do adm
 admin.site.register(Categoria)
-admin.site.register(Produto)
+admin.site.register(Produto, ProdutoAdmin)
