80a85151775a864c38e4640b3141c23c2690650d
Marco Antonio Cavalcanti da Silva <20231011110033@ifrn.local>
2025-06-02 15:48:33 -0300

    aula 12 - trabalhando com templates

--- Loja/loja/views/HomeView.py
+++ Loja/loja/views/HomeView.py
@@ -1,5 +1,15 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
+from loja.models import Produto
 def home_view(request):
+    
+    produto = request.GET.get("produto")
+    produtos = Produto.objects.all()
+    if produto is not None:
+        produtos = produtos.filter(Produto__contains=produto)
+    context = {
+        'produtos': produtos
+    }
+    return render(request, template_name='home/home.html', context=context, status=200)
     # return HttpResponse('<h1>Olá mundo!</h1>')
-    return render(request, template_name='home/home.html', status=200)
+    # return render(request, template_name='home/home.html', status=200)
--- Loja/loja/views/ProdutoView.py
+++ Loja/loja/views/ProdutoView.py
@@ -1,7 +1,18 @@
+from django.shortcuts import render # Retire from django.http import HttpResponse
+from loja.models import Produto
 from django.http import HttpResponse
 from loja.models import Produto
 from datetime import timedelta, datetime
 from django.utils import timezone
+
+def edit_produto_view(request, id=None):
+    produtos = Produto.objects.all()
+    if id is not None:
+        produtos = produtos.filter(id=id)
+    produto = produtos.first()
+    print(produto)
+    context = { 'produto': produto }
+    return render(request, template_name='produto/produto-edit.html', context=context, status=200) 
 
 
 def list_produto_view(request, id=None):
@@ -12,6 +23,7 @@ def list_produto_view(request, id=None):
     categoria = request.GET.get("categoria")
     fabricante = request.GET.get("fabricante")
     dias = request.GET.get("dias")
+    produtos = Produto.objects.all()
     
     #Mostra no console
     if destaque is not None:
@@ -46,8 +58,13 @@ def list_produto_view(request, id=None):
         now = now - timedelta(days = int(dias))
         produtos = produtos.filter(criado_em__gte=now)
     #Mostra no console
-    print(produtos)
 
-    if id is None:
-        return HttpResponse('<h1>Nenhum id foi informado</h1>')
-    return HttpResponse('<h1>Produto de id %s!</h1>' % id)
+    # Define o contexto e carrega o template
+    context = {'produtos': produtos}
+    return render(request, template_name='produto/produto.html',context=context, status=200)
+    
+    # print(produtos)
+
+    # if id is None:
+    #     return HttpResponse('<h1>Nenhum id foi informado</h1>')
+    # return HttpResponse('<h1>Produto de id %s!</h1>' % id)
