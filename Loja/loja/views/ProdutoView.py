9b1efe09937d559cce8d2a9b3276e4c44ce023cd
Marco Antonio Cavalcanti da Silva <20231011110033@ifrn.local>
2025-06-10 14:33:55 -0300

    aula 13 - trabalhando com templates

--- Loja/loja/views/ProdutoView.py
+++ Loja/loja/views/ProdutoView.py
@@ -1,9 +1,36 @@
-from django.shortcuts import render # Retire from django.http import HttpResponse
+from django.shortcuts import render, redirect # Retire from django.http import HttpResponse
 from loja.models import Produto
 from django.http import HttpResponse
 from loja.models import Produto
 from datetime import timedelta, datetime
 from django.utils import timezone
+
+def edit_produto_postback(request, id=None):
+    if request.method == 'POST':
+        # Salva dados editados
+        id = request.POST.get("id")
+        produto = request.POST.get("Produto")
+        destaque = request.POST.get("destaque")
+        promocao = request.POST.get("promocao")
+        msgPromocao = request.POST.get("msgPromocao")
+        print("postback")
+        print(id)
+        print(produto)
+        print(destaque)
+        print(promocao)
+        print(msgPromocao)
+        try:
+            obj_produto = Produto.objects.filter(id=id).first()
+            obj_produto.Produto = produto
+            obj_produto.destaque = (destaque is not None)
+            obj_produto.promocao = (promocao is not None)
+            if msgPromocao is not None:
+                obj_produto.msgPromocao = msgPromocao
+                obj_produto.save()
+                print("Produto %s salvo com sucesso" % produto)
+        except Exception as e:
+                print("Erro salvando edição de produto: %s" % e)
+    return redirect("/produto")
 
 def edit_produto_view(request, id=None):
     produtos = Produto.objects.all()
@@ -62,9 +89,50 @@ def list_produto_view(request, id=None):
     # Define o contexto e carrega o template
     context = {'produtos': produtos}
     return render(request, template_name='produto/produto.html',context=context, status=200)
-    
+
+
     # print(produtos)
 
     # if id is None:
     #     return HttpResponse('<h1>Nenhum id foi informado</h1>')
     # return HttpResponse('<h1>Produto de id %s!</h1>' % id)
+
+
+def details_produto_view(request, id=None):
+    # Processa o evento GET gerado pela action
+    produtos = Produto.objects.all()
+    if id is not None:
+        produtos = produtos.filter(id=id)
+    produto = produtos.first()
+    print(produto)
+    context = {'produto': produto}
+    return render(request, template_name='produto/produto-details.html', context=context, status=200)
+
+def delete_produto_view(request, id=None):
+    # Processa o evento GET gerado pela action
+    produtos = Produto.objects.all()
+    if id is not None:
+        produtos = produtos.filter(id=id)
+    produto = produtos.first()
+    print(produto)
+    context = {'produto': produto}
+    return render(request, template_name='produto/produto-delete.html', context=context, status=200)
+
+# adicione a função que trata o postback da interface de exclusão
+def delete_produto_postback(request, id=None):
+    # Processa o post back gerado pela action
+    if request.method == 'POST':
+    # Salva dados editados
+        id = request.POST.get("id")
+        produto = request.POST.get("Produto")
+        print("postback-delete")
+        print(id)
+        try:
+            Produto.objects.filter(id=id).delete()
+            print("Produto %s excluido com sucesso" % produto)
+        except Exception as e:
+            print("Erro salvando edição de produto: %s" % e)
+    return redirect("/produto")
+
+def create_produto_view(request, id=None):
+    return render(request, template_name='produto/produto-create.html',status=200)
